4eabec70d88648503f1ce1b12bdb13864f3b0334
izzibizz <contact@izabellind.com>
2025-04-29 18:11:31 +0200

    vide mobile

--- src/components/VideoBg.tsx
+++ src/components/VideoBg.tsx
@@ -1,7 +1,20 @@
-
+import { useEffect, useState }from "react"
 
 export const VideoBg = () => {
 
+    const [ isLaptop, setIsLaptop ] = useState(window.innerWidth > 1024)
+  
+    useEffect(() => {
+      const handleResize = () => {
+        setIsLaptop(window.innerWidth > 1024);
+      };
+  
+      handleResize();
+  
+      window.addEventListener("resize", handleResize);
+  
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
   return (
      <video
@@ -12,7 +25,7 @@ export const VideoBg = () => {
         className={`hide-controls absolute inset-0 w-full h-full max-h-[120vh] laptop:max-h-[95vh] object-cover z-0`}
       >
         <source
-          src="https://res.cloudinary.com/dlp85vjwx/video/upload/v1745864887/Sequence_01_5_gkogqj.mp4"
+          src={ isLaptop? "https://res.cloudinary.com/dlp85vjwx/video/upload/v1745864887/Sequence_01_5_gkogqj.mp4" : "https://res.cloudinary.com/dlp85vjwx/video/upload/v1745942988/Sequence_01_7_ydjmi4.mp4"}
           type="video/mp4"
         />
       </video>
