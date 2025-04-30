88b1ac9a0f91fbd49cbbeaa25c09462ef2845117
izzibizz <contact@izabellind.com>
2025-04-30 20:51:57 +0200

    fixed swiper vert

--- src/components/ClientsOverview.tsx
+++ src/components/ClientsOverview.tsx
@@ -8,13 +8,14 @@ export const ClientsOverview: React.FC = () => {
       <div className="w-10/12 laptop:w-9/12 mx-auto py-30 laptop:pb-48 flex flex-col gap-10 laptop:gap-0">
         <SwiperComp />
         <div className="flex flex-col gap-6">
-          <div className="flex flex-col gap-10">
-            <h3 className="font-header text-2xl tablet:text-4xl laptop:text-[50px] text-stone-700">
+          <div className="flex flex-col gap-2">
+            <h3 className="font-header text-2xl tablet:text-4xl laptop:text-[40px] text-stone-700">
               Våra Kunder
             </h3>
             <h4 className="font-heading italic text-lg tablet:text-2xl">
               Vi skapar unika uttryck för varje kund
             </h4>
+            </div>
             <p className="laptop:w-1/2 text-justify font-body">
               Varje projekt är en skräddarsydd upplevelse, formad utifrån våra
               kunders vision, målgrupp och identitet. Våra kunder är otroligt
@@ -30,27 +31,7 @@ export const ClientsOverview: React.FC = () => {
             >
               Se alla projekt
             </button>
-          </div>
-          <div className="flex flex-col gap-6">
-            <h4 className="font-heading italic text-lg tablet:text-2xl">
-              Vi skapar unika uttryck för varje kund
-            </h4>
-            <p className="laptop:w-1/2 text-justify font-body">
-              Varje projekt är en skräddarsydd upplevelse, formad utifrån våra
-              kunders vision, målgrupp och identitet. Våra kunder är otroligt
-              viktiga för oss, vårt mål är att hjälpa dem förstå och formulera
-              deras vision och sen förverkliga den. Med stark känsla för form,
-              tonalitet och strategi bygger itFlows digitala lösningar som
-              sticker ut och som stannar kvar i minnet. Här ser du ett urval av
-              våra uppdrag.
-            </p>
-            <button
-              className="bg-dark-blue self-center tablet:self-start p-3 px-6 rounded-full w-fit h-fit text-sm cursor-pointer shadow-lg text-white font-body hover:scale-110"
-              onClick={() => navigate("/kunder")}
-            >
-              Se alla projekt
-            </button>
-          </div>
+
         </div>
       </div>
     </section>
--- src/components/SwiperComp.tsx
+++ src/components/SwiperComp.tsx
@@ -1,14 +1,11 @@
-
-
-import projectsJson from "../data/projectsData.json"
-import { Swiper, SwiperSlide } from 'swiper/react';
+import projectsJson from "../data/projectsData.json";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { useNavigate } from "react-router-dom";
 
-import { EffectCoverflow, Pagination } from 'swiper/modules';
-import '../styles/swiper.css';
-import '../styles/effect-coverflow.css';
-import '../styles/pagination.css'; 
-
+import { EffectCoverflow, Pagination } from "swiper/modules";
+import "../styles/swiper.css";
+import "../styles/effect-coverflow.css";
+import "../styles/pagination.css";
 
 type Project = {
   name: string;
@@ -23,43 +20,61 @@ type Project = {
 const projects: Project[] = projectsJson;
 
 export const SwiperComp: React.FC = () => {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
-
-  
-    return (
-        <div className="overflow-visible w-full flex flex-col">
-          <Swiper
-            effect={'coverflow'}
-            grabCursor={true}
-            centeredSlides={true}
-            slidesPerView={1.5}
-            loop={true}
-            coverflowEffect={{
-              rotate: 50,
-              stretch: 80,
-              depth: 100,
-              modifier: 1,
-              slideShadows: false,
-            }}
-            pagination={true}
-            modules={[EffectCoverflow, Pagination]}
-            className="w-full"
+  return (
+    <div className="overflow-visible w-full flex flex-col">
+      <Swiper
+        effect={"coverflow"}
+        direction={"horizontal"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={1.5}
+        breakpoints={{
+          320: {
+            direction: "vertical",
+          },
+          640: {
+            direction: "horizontal",
+          },
+          1024: {
+            direction: "horizontal",
+          },
+        }}
+        loop={true}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 80,
+          depth: 100,
+          modifier: 1,
+          slideShadows: false,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="w-full h-[300px] tablet:h-auto"
+      >
+        {projects.map((project, index) => (
+          <SwiperSlide
+            key={index}
+            className=" bg-warm-white"
+            onClick={() => navigate("/kunder")}
           >
-            { projects.map((project, index) => (
-            <SwiperSlide key={index} className=" bg-warm-white" onClick={() => navigate("/kunder")}>
-            <img src={project.pictures[0]} className="pb-10"/>
+            <img src={project.pictures[0]} className="pb-10" />
             <div className="font-body self-end text-end hidden tablet:block">
-            <a href={project.website} target="_blank" rel="noref noopener" className="text-xl">{project.company}</a>
-            <p className="text-sm">{project.title}</p>
-            <p className="text-sm">{project.year}</p>
-          </div>
+              <a
+                href={project.website}
+                target="_blank"
+                rel="noref noopener"
+                className="text-xl"
+              >
+                {project.company}
+              </a>
+              <p className="text-sm">{project.title}</p>
+              <p className="text-sm">{project.year}</p>
+            </div>
           </SwiperSlide>
-            ))}
-
-          </Swiper>
-        </div>
-      );
-    }
-
-
+        ))}
+      </Swiper>
+    </div>
+  );
+};
