8eaae123c98ce65d3604e8df3ae4fa9bd3d7cfa7
izzibizz <contact@izabellind.com>
2025-04-30 07:34:21 +0200

    framer-motion

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 import { Testimonial } from "./Testimonial";
 import { VideoBg } from "./VideoBg.js";
 import testimonials from "../data/testimonials.json";
@@ -11,19 +12,33 @@ export const Hero: React.FC = () => {
       <VideoBg />
       <div className="relative pt-42 laptop:pt-48 w-10/12 mx-auto flex flex-col gap-6 laptop:gap-16 mb-20 laptop:mb-0 laptop:h-[95vh]">
         <div className="text-white">
-          <h1 className="text-[50px] laptop:text-[90px] leading-tight font-bold">
+          <motion.h1
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 1, duration: 0.6, ease: "easeOut" }}
+            className="text-[50px] laptop:text-[90px] leading-tight font-bold"
+          >
             BRANDING, DESIGN & WEBBUTVECKLING
             <br />
             <span className="text-2xl laptop:text-[70px]">
               {" "}
               enkelt, snyggt och skräddarsytt för dig
             </span>
-          </h1>
-          <h2 className="text-xl laptop:text-[40px] font-body text-light tracking-wide italic">
+          </motion.h1>
+          <motion.h2
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 1.5, duration: 0.7, ease: "easeOut" }}
+            className="text-xl laptop:text-[40px] font-body text-light tracking-wide italic"
+          >
             Vi bygger. Du växer.
-          </h2>
+          </motion.h2>
         </div>
-        <div className="flex flex-col gap-16 laptop:gap-10 laptop:flex-row justify-between">
+        <motion.div 
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 4, duration: 2 }}
+        className="flex flex-col gap-16 laptop:gap-10 laptop:flex-row justify-between">
           <div className="flex gap-4 laptop:gap-10">
             <button
               className="bg-red-beige text-white laptop:text-warm-black p-3 px-6 rounded-full w-fit h-fit text-xs laptop:text-xl cursor-pointer shadow-lg hover:scale-110 hover:text-white"
@@ -39,7 +54,7 @@ export const Hero: React.FC = () => {
             </button>
           </div>
           <Testimonial testimonial={testimonials[0]} position={"self-end"} />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
