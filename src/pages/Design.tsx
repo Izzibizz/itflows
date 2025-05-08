ac8216b3abbf4071ab8ae5879130936a89b58440
izzibizz <contact@izabellind.com>
2025-05-08 16:22:53 +0200

    process

--- src/pages/Design.tsx
+++ src/pages/Design.tsx
@@ -13,7 +13,6 @@ export const Design: React.FC = () => {
     return (
       <section className="animate-fadeIn ">
      <ServiceComp circleColors= " rgb(255, 246, 226), rgb(163, 184, 213), rgb(159, 154, 189), rgb(101, 136, 184))" textColor="text-light-beige" />
-     <img src="https://res.cloudinary.com/dlp85vjwx/image/upload/v1746693489/47356d71637fef8545556703025dbab5_qtjzyu.jpg" className="w-full h-full object-cover"/>
       </section>
     )
   }
--- src/pages/WebbUtveckling.tsx
+++ src/pages/WebbUtveckling.tsx
@@ -15,6 +15,7 @@ export const WebbUtveckling: React.FC = () => {
   return (
       <section className="animate-fadeIn flex flex-col gap-14 laptop:gap-18 font-body">
       <ServiceComp  circleColors=" rgb(255, 246, 226), rgb(250, 183, 152), rgb(238, 142, 113), rgb(255, 111, 106))" textColor="text-dark-blue" />
+      <img src={window.innerWidth < 1025 ? "https://res.cloudinary.com/dlp85vjwx/image/upload/v1746710652/process-mobile_i7yalp.svg" :"https://res.cloudinary.com/dlp85vjwx/image/upload/v1746711179/process-laptop_l2nrev.svg"} className="w-full laptop:w-2/3 mx-auto object-cover" />
       </section>
   )
 }
