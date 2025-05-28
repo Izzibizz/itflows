8cedfbd6843dd461cf02a014613185bfa847a390
izzibizz <contact@izabellind.com>
2025-05-28 22:03:07 +0200

    prislista

--- src/samarbete/components/PrisInfo.tsx
+++ src/samarbete/components/PrisInfo.tsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
+import paketData from "../../data/prislista.json"
+
+type Package = {
+  title: string;
+  price: string;
+};
+
+const paketInfo: Package[] = paketData;
 
 export const PrisInfo = () => {
 
+ const [openStates, setOpenStates] = useState([false, false, false]);
+
+
+ const toggleOpen = (index: number) => {
+  setOpenStates((prev) =>
+    prev.map((isOpen, i) => (i === index ? !isOpen : isOpen))
+  );
+};
   return (
     <section className="flex flex-col gap-10">
       <h3 className="font-collab text-4xl desktop:text-[40px]">
@@ -12,36 +29,42 @@ export const PrisInfo = () => {
         ser till att känslan och resultatet levererar.
       </h4>
       <div className="flex flex-col desktop:w-11/12">
-        <div className="flex border-t border-dotted">
-          <div className="border border-dotted p-6 rounded-full h-[150px] w-[150px] items-center flex justify-center text-[60px] font-collab">
-            1
+       {paketInfo.map((paket, index) => (
+          <div
+            key={index}
+            className={`flex ${
+              index === paketInfo.length - 1
+                ? "border-y"
+                : "border-t"
+            } border-dotted`}
+          >
+            <div className="border border-dotted p-6 rounded-full h-[150px] w-[150px] items-center flex justify-center text-[60px] font-collab">
+              {index + 1}
+            </div>
+            <div className="p-6 flex flex-col gap-2">
+              <h3 className="font-collab text-2xl">{paket.title}</h3>
+              <h4>Pris: {paket.price}</h4>
+              <button
+                onClick={() => toggleOpen(index)}
+                className="flex gap-3 items-center laptop:hidden cursor-pointer"
+              >
+                <p>Vad som ingår</p>
+                <IoIosArrowDown
+                  className={`transition-transform duration-300 ${
+                    openStates[index] ? "rotate-180" : ""
+                  }`}
+                />
+              </button>
+              {openStates[index] && (
+                <ul className="pt-2 text-sm">
+                  <li>✔ Punkt 1</li>
+                  <li>✔ Punkt 2</li>
+                  <li>✔ Punkt 3</li>
+                </ul>
+              )}
+            </div>
           </div>
-          <div className="p-6">
-            <h3 className="font-collab text-2xl">Lilla</h3>
-            <h4>Pris: 29 995 kr</h4>
-            <div className="flex gap-3 items-center laptop:hidden"><p>Vad som ingår</p><IoIosArrowDown /></div>
-          </div>
-        </div>
-        <div className="flex border-t border-dotted">
-          <div className="border border-dotted p-6 rounded-full h-[150px] w-[150px] items-center flex justify-center text-[60px] font-collab">
-            2
-          </div>
-          <div className="p-6">
-             <h3 className="font-collab text-2xl">Mellan</h3>
-            <h4>Pris: 38 995 kr</h4>
-            <div className="flex gap-3 items-center laptop:hidden"><p>Vad som ingår</p><IoIosArrowDown /></div>
-          </div>
-        </div>
-        <div className="flex border-y border-dotted">
-          <div className="border border-dotted p-6 rounded-full h-[150px] w-[150px] items-center flex justify-center text-[60px] font-collab">
-            3
-          </div>
-          <div className="p-6">
-             <h3 className="font-collab text-2xl">Stora</h3>
-            <h4>Pris: 55 995 kr</h4>
-            <div className="flex gap-3 items-center laptop:hidden"><p>Vad som ingår</p><IoIosArrowDown /></div>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
