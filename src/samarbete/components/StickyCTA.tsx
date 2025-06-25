2e65954b50137dfbb9fcc2b2173c27a5cabea995
izzibizz <contact@izabellind.com>
2025-06-25 11:49:01 +0200

    format for mobile!

--- src/samarbete/components/StickyCTA.tsx
+++ src/samarbete/components/StickyCTA.tsx
@@ -8,7 +8,7 @@ import { useCollabStore } from "../../stores/useCollabStore";
 export const StickyCTA: React.FC = () => {
   const [isPopup, setIsPopup] = useState(false);
   const popupRef = useRef<HTMLDivElement | null>(null);
-  const [hideSticky, setHideSticky] = useState(false)
+  const [hideSticky, setHideSticky] = useState(false);
   const { popupHasBeenSeen, setPopupHasBeenSeen } = useCollabStore();
   const [state, handleSubmit] = useForm("xkgbqlnz");
 
@@ -18,9 +18,9 @@ export const StickyCTA: React.FC = () => {
   };
 
   const bookingClick = () => {
-    setIsPopup(true)
-    setPopupHasBeenSeen(false)
-  }
+    setIsPopup(true);
+    setPopupHasBeenSeen(false);
+  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -28,12 +28,12 @@ export const StickyCTA: React.FC = () => {
     }, 30000);
   }, []);
 
-  useEffect(() => {
+ useEffect(() => {
     if (state.succeeded) {
       setTimeout(() => {
         setIsPopup(false);
         setPopupHasBeenSeen(true);
-        setHideSticky(true)
+        setHideSticky(true);
       }, 5000);
     }
   }, [state, setPopupHasBeenSeen]);
@@ -62,12 +62,13 @@ export const StickyCTA: React.FC = () => {
     };
   }, []);
 
-  console.log(state.succeeded);
+
+  console.log(state.succeeded, setHideSticky);
   return (
     <section
       className={`animate-fadeIn ${hideSticky && "hidden"} ${
         isPopup && !popupHasBeenSeen
-          ? "w-screen h-screen bg-warm-black/90 tablet:bg-warm-black/70 text-warm-black z-80 "
+          ? "w-screen h-screen bg-warm-black/70 text-warm-black z-80 "
           : " rounded-tl-3xl hover:scale-110 laptop:origin-right bg-collab-red hover:bg-collab-green p-3 px-6  cursor-pointer text-warm-white z-50"
       } fixed bottom-0 right-0 laptop:text-lg text-sm `}
     >
@@ -76,41 +77,43 @@ export const StickyCTA: React.FC = () => {
           className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-11/12 laptop:w-1/2 laptop:max-w-[1000px] animate-fadeIn rounded-4xl flex flex-col bg-warm-white"
           ref={popupRef}
         >
-          <div className="flex flex-col tablet:flex-row justify-between relative">
+          <div className="flex flex-col tablet:flex-row justify-between relative ">
             <RxCross1
               className="absolute z-60 top-4 right-4 w-6 h-6 laptop:top-8 laptop:right-8 text-warm-black cursor-pointer"
               onClick={() => closePopup()}
             />
             {state.succeeded ? (
-              <div className="animate-fadeIn flex flex-col gap-2 p-6 tablet:w-2/3 items-center text-center justify-center">
+              <div className="animate-fadeIn flex flex-col gap-2 h-full p-6 tablet:w-2/3 items-center text-center justify-center  min-h-[500px] ">
                 <h3 className="font-collab text-4xl">Tack för din bokning!</h3>
                 <p>Vi hör av oss inom kort.</p>
               </div>
             ) : (
               <form
                 onSubmit={handleSubmit}
-                className="relative flex flex-col gap-10 bg-warm-white p-6 laptop:p-8 rounded-t-4xl tablet:rounded-l-4xl tablet:rounded-r-none tablet:w-2/3"
+                className="relative flex flex-col gap-10 bg-warm-white p-6 laptop:p-8 rounded-t-4xl rounded-b-4xl tablet:rounded-b-none tablet:rounded-l-4xl tablet:rounded-r-none tablet:w-2/3"
               >
                 <div className="flex flex-col gap-2 items-center self-center">
-                  <h3 className="text-2xl">
+                  <h3 className="text-xl tablet:text-2xl">
                     <span className="underline">Just nu</span> erbjuder vi en{" "}
                     <br />{" "}
                     <span className="font-semibold font-collab text-4xl tablet:text-3xl laptop:text-[50px]">
                       gratis
                     </span>{" "}
-                    <span className="text-4xl tablet:text-3xl laptop:text-4xl font-collab">
+                    <span className="text-2xl tablet:text-3xl laptop:text-4xl font-collab">
                       första konsultation
                     </span>
                   </h3>
-                  <h4 className="text-2xl self-end">helt utan bindning</h4>
+                  <h4 className="text-xl tablet:text-2xl self-end">
+                    helt utan bindning
+                  </h4>
                 </div>
                 <div className="flex flex-col gap-2">
                   <label className="flex gap-2">
-                    Namn:
+                    <span className="whitespace-nowrap">Namn:</span>
                     <input
                       type="text"
                       name="name"
-                      className="border border-dotted border-collab-green rounded-lg"
+                      className="border border-dotted border-collab-green rounded-lg w-full"
                       required
                     />
                   </label>
@@ -120,11 +123,11 @@ export const StickyCTA: React.FC = () => {
                     errors={state.errors}
                   />
                   <label className="flex gap-2">
-                    Företag:
+                    <span className="whitespace-nowrap">Företag:</span>
                     <input
                       type="text"
                       name="företag"
-                      className="border border-dotted border-collab-green rounded-lg"
+                      className="border border-dotted border-collab-green rounded-lg w-full"
                       required
                     />
                   </label>
@@ -134,12 +137,12 @@ export const StickyCTA: React.FC = () => {
                     errors={state.errors}
                   />
                   <label className="flex gap-2">
-                    E-post:
+                    <span className="whitespace-nowrap">E-post:</span>
                     <input
                       id="email"
                       type="email"
                       name="email"
-                      className="border border-dotted border-collab-green rounded-lg"
+                      className="border border-dotted border-collab-green rounded-lg w-full"
                       required
                     />
                   </label>
@@ -148,11 +151,11 @@ export const StickyCTA: React.FC = () => {
                     field="email"
                     errors={state.errors}
                   />
-                  <label className="flex flex-col gap-2">
+                  <label className="flex flex-col gap-2 w-full">
                     Meddelande:
                     <textarea
                       name="message"
-                      className="border p-2 border-dotted border-collab-green rounded-lg"
+                      className="border p-2 border-dotted border-collab-green rounded-lg w-full"
                       required
                     />
                     <ValidationError
@@ -168,8 +171,6 @@ export const StickyCTA: React.FC = () => {
                   >
                     Skicka
                   </button>
-                </div>
-                <div className="">
                   <p className="text-xs tablet:text-sm tablet:text-justify">
                     Du får en konsultation värd 2 500 kr – helt gratis! <br /> I
                     ett 30-minuters onlinesamtal analyserar vi ditt/ert företags
@@ -196,15 +197,15 @@ export const StickyCTA: React.FC = () => {
               muted
               loop
               playsInline
-              className="w-full aspect-[4/3] tablet:w-1/2 tablet:aspect-[3/4] laptop:h-full object-cover object-[center_35%] tablet:object-center rounded-b-4xl tablet:rounded-r-4xl tablet:rounded-l-none"
+              className="hidden tablet:block tablet:w-1/2 tablet:aspect-[3/4] laptop:h-full object-cover object-[center_35%] tablet:object-center rounded-b-4xl tablet:rounded-r-4xl tablet:rounded-l-none"
             />
           </div>
         </div>
-      ) :  (
+      ) : (
         <p
           className="flex gap-2 font-light items-center
         "
-        onClick={() => bookingClick()}
+          onClick={() => bookingClick()}
         >
           Boka gratis konsultation
         </p>
